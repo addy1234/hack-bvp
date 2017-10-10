381e7a94308380c2c2e5585f01ee4ce16e90b8c5
Jatin Bindra <jatinbindra171998@gmail.com>
2017-10-11 01:45:21 +0530

    dev

---
+++ Android-App/app/src/main/java/com/developer/jatin/hack_bvp/splash.java
@@ -0,0 +1,31 @@
+package com.developer.jatin.hack_bvp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class splash extends Activity {
+//    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(splash.this,MainScreen.class);
+                splash.this.startActivity(mainIntent);
+                splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
